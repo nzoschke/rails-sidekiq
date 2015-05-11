a3624d5b5ea4ddfdb310699e9e04e2a40e249f36
Noah Zoschke <nzoschke@gmail.com>
2015-05-11 15:22:40 -0700

    use convox/postgres and convox/redis; configure activerecord and sidekiq for additional environment

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV['REDIS_PORT_6379_TCP_ADDR']}:#{ENV['REDIS_PORT_6379_TCP_PORT']}/0" }
+  config.redis = { url: "redis://:#{ENV['REDIS_ENV_REDIS_PASSWORD']}@#{ENV['REDIS_PORT_6379_TCP_ADDR']}:#{ENV['REDIS_PORT_6379_TCP_PORT']}/#{ENV['REDIS_ENV_REDIS_DATABASE']}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{ENV['REDIS_PORT_6379_TCP_ADDR']}:#{ENV['REDIS_PORT_6379_TCP_PORT']}/0" }
+  config.redis = { url: "redis://:#{ENV['REDIS_ENV_REDIS_PASSWORD']}@#{ENV['REDIS_PORT_6379_TCP_ADDR']}:#{ENV['REDIS_PORT_6379_TCP_PORT']}/#{ENV['REDIS_ENV_REDIS_DATABASE']}" }
 end
